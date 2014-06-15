cc96c9ec733916f86928bf17127f1ea9bc8f5c1c
Torndeco <torndeco.di@gmail.com>
2014-06-15 16:56:29 +0100

    Minor Perf Tweaks
    Also fix for outputsize

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,20 @@
-
+/*
+* Copyright (C) 2014 Declan Ireland <http://github.com/torndeco/extDB>
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+*/
 
 
 #include <cstdlib>
@@ -18,13 +34,13 @@ namespace {
 	static void __attribute__((constructor))
 	extension_init(void)
 	{
-		std::cout << "extDB shared library init" << std::endl;
+		//std::cout << "extDB shared library init" << std::endl;
 	}
 
 	static void __attribute__((destructor))
 	extension_destroy(void)
 	{
-		std::cout << "extDB shared library destroy" << std::endl;
+		//std::cout << "extDB shared library destroy" << std::endl;
 	}
 
 	extern "C"
@@ -34,7 +50,8 @@ namespace {
 
 	void RVExtension(char *output, int outputSize, const char *function)
 	{
-	  extension.callExtenion(output, outputSize, function);
+		outputSize -= 1;
+		extension.callExtenion(output, outputSize, function);
 	};
 
 #elif _MSC_VER
@@ -57,12 +74,13 @@ namespace {
 
 	extern "C"
 	{
-	  __declspec(dllexport) void __stdcall RVExtension(char *output, int outputSize, const char *function); 
+		__declspec(dllexport) void __stdcall RVExtension(char *output, int outputSize, const char *function); 
 	};
 
 	void __stdcall RVExtension(char *output, int outputSize, const char *function)
 	{
-	  extension.callExtenion(output,outputSize,function);
+		outputSize -= 1;
+		extension.callExtenion(output,outputSize,function);
 	};
 
 #elif __MINGW32__
---
+++ src/protocols/db_basic.cpp
@@ -0,0 +1,110 @@
+/*
+* Copyright (C) 2014 Declan Ireland <http://github.com/torndeco/extDB>
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+*/
+
+
+#include "db_raw.h"
+
+#include <Poco/Data/Common.h>
+#include <Poco/Data/MetaColumn.h>
+#include <Poco/Data/RecordSet.h>
+#include <Poco/Data/Session.h>
+#include <Poco/Exception.h>
+
+#include <cstdlib>
+#include <iostream>
+
+
+std::string DB_BASIC::updatePlayerPos(AbstractExt *extension, std::string steamid, std::string pos)
+{
+	// FOO
+	return "";
+}
+
+std::string DB_BASIC::updatePlayerInventory(AbstractExt *extension, std::string steamid, std::string inventory)
+{
+	// FOO
+	return "";
+}
+
+std::string DB_BASIC::updatePlayerHealth(AbstractExt *extension, std::string steamid, std::string health)
+{
+	// FOO
+	return "";
+}
+
+std::string DB_BASIC::updatePlayerDied(AbstractExt *extension, std::string steamid)
+{
+	// FOO
+	return "";
+}
+
+std::string DB_BASIC::updatePlayerSpawn(AbstractExt *extension, std::string steamid)
+{
+	// FOO
+	return "";
+}
+
+std::string DB_BASIC::callPlugin(AbstractExt *extension, std::string input_str)
+{
+
+    try
+    {
+		std::string result;
+		Poco::Data::Session db_session = extension->getDBSession_mutexlock();
+		Poco::Data::Statement select(db_session);
+		select << input_str;
+		select.execute();
+		Poco::Data::RecordSet rs(select);
+		if (rs.columnCount() >= 1)
+		{
+			std::size_t cols = rs.columnCount();
+			bool more = rs.moveFirst();
+			while (more)
+			{
+				result += " [";
+				for (std::size_t col = 0; col < cols; ++col)
+				{
+					if (rs.columnType(col) == Poco::Data::MetaColumn::FDT_STRING)
+					{
+						result += "\"" + (rs[col].convert<std::string>() + "\"" + ", ");
+					}
+					else
+					{
+						result += (rs[col].convert<std::string>() + ", ");
+					}
+				}
+
+				more = rs.moveNext();
+				if (more)
+				{
+					result += "],";
+				}
+				else
+				{
+					result = result.substr(0, (result.length() - 2)) + "]";
+				}
+			}
+		}
+		return result;
+	}
+    catch (Poco::Exception& e)
+    {
+        return "[\"ERROR\",\"Error\"]";
+        std::cout << "extDB: Error: " << e.displayText() << std::endl;
+    }
+}
