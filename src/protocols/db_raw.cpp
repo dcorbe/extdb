1730d50fe5305b776084d108ab2ab58585cda1d1
Torndeco <torndeco.di@gmail.com>
2014-07-10 10:05:57 +0100

    Renamed Folders + Updated V10 with fix for DB_PROCEDURE
    Note this doesn't effect Atlis Life RPG

--- src/protocols/db_raw.cpp
+++ src/protocols/db_raw.cpp
@@ -15,8 +15,6 @@ You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
-
-
 #include "db_raw.h"
 
 #include <Poco/Data/Common.h>
@@ -27,13 +25,12 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 #include "Poco/Data/MySQL/Connector.h"
 #include "Poco/Data/MySQL/MySQLException.h"
-/*
 #include "Poco/Data/SQLite/Connector.h"
 #include "Poco/Data/SQLite/SQLiteException.h"
 #include "Poco/Data/SQLite/Connector.h"
 #include "Poco/Data/SQLite/SQLiteException.h"
 #include "Poco/Data/ODBC/Connector.h"
-#include "Poco/Data/ODBC/ODBCException.h"*/
+#include "Poco/Data/ODBC/ODBCException.h"
 
 #include <cstdlib>
 #include <iostream>
