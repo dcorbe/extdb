ed05d702af3f27979f3f373020574cd88cafddf4
Torndeco <torndeco.di@gmail.com>
2014-08-10 01:03:14 +0100

    Test Build for V15

--- src/ext.cpp
+++ src/ext.cpp
@@ -433,7 +433,7 @@ void Ext::connectDatabase(char *output, const int &output_size, const std::strin
 
 std::string Ext::version() const
 {
-    return "15";
+    return "15 - dev build 10/08/14";
 }
 
 
